d490c87bf02c784b78cf7a4fec5f6dc96f5653ab
laxman gurung <gurungjavac@gmail.com>
2022-05-18 22:34:07 -0500

    first version

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+       int value1 = 10;
+       int value2 = 5;
+       int result = value1* value2;
+        System.out.println("multiplication " +result);
+        result = value1/ value2;
+        System.out.println("division " + result);
+        result +=5;
+        System.out.println("addition " + result);
+        result++;
+        System.out.println("increment operator " + result);
+
+        }
+
+
+
+    }
